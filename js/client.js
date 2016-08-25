9a21b79f03c53767e79b3263d8f1380e0bd174d6
CodeHimanshu <himanshuagrawal1998@gmail.com>
2016-08-25 18:13:25 +0530

    jQuery effects added

--- js/client.js
+++ js/client.js
@@ -21,7 +21,47 @@ $(document).ready(function(){
 		$(this).hide();
 	});
 	$(".mouseenter").mouseenter(function(){
-		$(this).hide();
+		alert("You entered mouseenter");
 	});
-	
+	$(".mouseenter").mouseleave(function(){
+		alert("You now leave mouseenter");
+	});
+	$(".mousedown").mousedown(function(){
+		alert("you pressed mouse button");
+	});
+	$(".mousedown").mouseup(function(){
+		alert("you released mouse button");
+	});
+	$(".hover").hover(function(){
+		alert("You entered");
+	},
+	function(){
+		alert("You left");
+	});
+	$("input").focus(function(){
+		$(this).css("background-color", "#cccccc");
+	});
+	$("input").blur(function(){
+		$(this).css("background-color","#ffffff" );
+	});
+	$(".on").on({
+		mouseenter: function(){
+		alert("Mouseenter");
+	},
+		dblclick: function(){
+		$(this).hide();
+	}
+	});
+	$(".slowhide").click(function(){
+		$(this).hide(1000);
+	});
+	$(".toggle").click(function(){
+		$(".text").toggle();
+	})
+});
+$(window).resize(function(){
+	alert("Haaa Resizing");
+});
+$(window).scroll(function(){
+	console.log("scrolling");
 });
